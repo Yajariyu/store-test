5c9dda6789db418b643a82ee04adb280f7495a6a
Yajariyu Cardoza <yajariyu@gmail.com>
2023-04-15 00:07:30 -0400

    add title

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Head from "next/head";
 import Nabvar from "../ui/Nabvar/Nabvar";
 
 const Layout = ({ children }) => {
   return (
     <div className="w-full">
+      <Head>
+        <title>My Store</title>
+      </Head>
       <Nabvar />
       {children}
     </div>
