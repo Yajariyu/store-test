9d6fa684e13a3cc84e83b9d6b8ff58723e108632
Yajariyu Cardoza <yajariyu@gmail.com>
2023-04-22 17:13:11 -0400

    add flex in items

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -9,32 +9,34 @@ import ProductModal from "../ui/Modal/ProductModal";
 export const ProductCard = ({ product }) => {
   const [openModal, setOpenModal] = useState(false);
   return (
-    <div className=" flex flex-col w-full md:w-[300px] lg:w-[250px] min-h-[300px] px-4 border rounded-lg border-purple-600 ">
-      <div className="relative min-h-[100px] h-2/4 px-auto mt-6">
-        <Image
-          src={product.image}
-          alt={product.title}
-          fill
-          style={{ objectFit: "contain" }}
-          sizes="(max-width:80%)"
-        />
-      </div>
-      <div>
-        <ProductTitle title={product.title} id={product.id} />
-        <ProductPrice product={product} />
-        <div className="min-h-[150px] max-h-[150px]">
-          <p className="line-clamp-5">{product.description}</p>
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col w-full md:w-[300px] lg:w-[250px] min-h-[300px] px-4 border rounded-lg border-purple-600 ">
+        <div className="relative min-h-[100px] h-2/4 px-auto mt-6">
+          <Image
+            src={product.image}
+            alt={product.title}
+            fill
+            style={{ objectFit: "contain" }}
+            sizes="(max-width:80%)"
+          />
         </div>
+        <div>
+          <ProductTitle title={product.title} id={product.id} />
+          <ProductPrice product={product} />
+          <div className="min-h-[150px] max-h-[150px]">
+            <p className="line-clamp-5">{product.description}</p>
+          </div>
+        </div>
+        <span
+          onClick={() => setOpenModal(true)}
+          className="mb-4 text-center underline cursor-pointer"
+        >
+          Learn more
+        </span>
+        {openModal && (
+          <ProductModal product={product} setOpenModal={setOpenModal} />
+        )}
       </div>
-      <span
-        onClick={() => setOpenModal(true)}
-        className="mb-4 text-center underline cursor-pointer"
-      >
-        Learn more
-      </span>
-      {openModal && (
-        <ProductModal product={product} setOpenModal={setOpenModal} />
-      )}
     </div>
   );
 };
--- src/components/products/ProductGrid.jsx
+++ src/components/products/ProductGrid.jsx
@@ -4,12 +4,10 @@ import { ProductCard } from "./ProductCard";
 
 const ProductGrid = ({ products }) => {
   return (
-    <div className="w-full h-full flex justify-center">
-      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center items-center gap-6 p-6 ">
-        {products?.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+    <div className="mt-10 grid justitfy-center items-end first-line: grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-6 ">
+      {products?.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 };
